2e226680ca30d516940ca650140c04c8d0a747f6
wzc8961661 <916627105@qq.com>
2019-07-29 11:44:49 +0800

    planA_0729

---
+++ planA_dao/src/main/java/com/czxy/dao/UserMapper.java
@@ -0,0 +1,13 @@
+package com.czxy.dao;
+
+import com.czxy.domain.User;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author Lenovo
+ * @version v 1.0
+ * @date 2019/7/29
+ */
+@org.apache.ibatis.annotations.Mapper
+public interface UserMapper extends Mapper<User> {
+}
