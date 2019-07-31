dc554c26b3b6f33651d81865f079fc8b9d44210c
it1963758523 <1963758523@qq.com>
2019-07-31 08:10:33 +0800

    planA_0729

---
+++ planA_web/src/main/java/com/czxy/MyAapplication.java
@@ -0,0 +1,18 @@
+package com.czxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author 黄开文
+ * @Version v 1.0
+ * @Date 2019/7/29
+ */
+@SpringBootApplication
+public class MyAapplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyAapplication.class,args);
+    }
+
+}
